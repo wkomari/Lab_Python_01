d21a2c759ed948fff503ec60322c359be0a4e36f
william <omariwilliam2@gmail.com>
2012-06-20 12:58:25 +0000

    updatedpythonlab2

---
+++ userinput.py
@@ -0,0 +1,19 @@
+#python code for users
+# EXERCISE 4
+
+FN = 'Anne'
+LN = 'Willims'
+month = 'July'
+Day = '31'
+Year = '1980'
+DOB = month +' '+ Day +',' +'  '+ Year
+
+print 'Enter your first name :'+ FN 
+
+print 'Enter your last name :'+ LN 
+print 'Enter your date of birth:'
+print 'Month?'+ '  '+ month
+print 'Day?'+ '  '+ Day 
+print 'Year?'+ '  '+ Year
+print FN + '  '+LN+'  '+' was born on '+ DOB
+
