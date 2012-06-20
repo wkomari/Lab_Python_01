bb91c8264842f7c46ba429f9f14d87f6bfa46672
william <omariwilliam2@gmail.com>
2012-06-20 11:20:52 +0000

    mypythonlab2

---
+++ exercise.py
@@ -0,0 +1,88 @@
+Python 2.7.2+ (default, Oct  4 2011, 20:03:08) 
+[GCC 4.6.1] on linux2
+Type "copyright", "credits" or "license()" for more information.
+==== No Subprocess ====
+>>> x = 1
+>>> 56thnumber = 1
+SyntaxError: invalid syntax
+>>> 56thnumber = 'hello'
+SyntaxError: invalid syntax
+>>> NUMBER = 'hello'
+>>> length = 'hello'
+>>> length = 3
+>>> !Tayo! = 3
+SyntaxError: invalid syntax
+>>> Android_phone1 = 3
+>>> Android_phone1 = 'come'
+>>> this variable = 'come'
+SyntaxError: invalid syntax
+>>> a  False
+SyntaxError: invalid syntax
+>>> a = False
+>>> a = False
+>>> b = 3.7
+>>> a = False
+>>> type(a)
+<type 'bool'>
+>>> b=3.7
+>>> type(b)
+<type 'float'>
+>>> c = 'Alex'
+>>> type(c)
+<type 'str'>
+>>> d = 7
+>>> type(d)
+<type 'int'>
+>>> e = 'True'
+>>> type(e)
+<type 'str'>
+>>> g = '17
+SyntaxError: EOL while scanning string literal
+>>> h = True
+>>> type(h)
+<type 'bool'>
+>>> i = '2.222'
+>>> type(i)
+<type 'str'>
+>>> j = "--add--"
+>>> type(j)
+<type 'str'>
+>>> 
+>>> a = False
+>>> gg = False
+>>> hh = True
+>>> cc = False
+>>> b and c
+'Alex'
+>>> hh and cc
+False
+>>> 
+>>> 
+>>> 
+>>> 5/2
+2
+>>> gg = False
+>>> 5/2.0
+2.5
+>>> 5.0/2
+2.5
+>>> 7*(1/2)
+0
+>>> 7*(1/2)
+0
+>>> 7*(1/2.0)
+3.5
+>>> 5**2
+25
+>>> 
+>>> 5.0**2
+25.0
+>>> 5**2.0
+25.0
+>>> 1/3
+0
+>>> 1/3.0
+0.3333333333333333
+>>> 0.3333333333333333
+0.3333333333333333
+>>> 
